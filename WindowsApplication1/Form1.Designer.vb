02c0226d81d1366575282664aa90f496fefc20fa
MrCoult <80452894+MrCoult@users.noreply.github.com>
2021-03-11 13:53:50 +0200

    Add files via upload

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DatePicker = New System.Windows.Forms.DateTimePicker()
+        Me.btnLuna = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnZileSal = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DatePicker
+        '
+        Me.DatePicker.Location = New System.Drawing.Point(86, 85)
+        Me.DatePicker.Name = "DatePicker"
+        Me.DatePicker.Size = New System.Drawing.Size(200, 20)
+        Me.DatePicker.TabIndex = 0
+        '
+        'btnLuna
+        '
+        Me.btnLuna.Location = New System.Drawing.Point(96, 147)
+        Me.btnLuna.Name = "btnLuna"
+        Me.btnLuna.Size = New System.Drawing.Size(150, 23)
+        Me.btnLuna.TabIndex = 1
+        Me.btnLuna.Text = "zile luna curenta"
+        Me.btnLuna.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(146, 248)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'btnZileSal
+        '
+        Me.btnZileSal.Location = New System.Drawing.Point(96, 196)
+        Me.btnZileSal.Name = "btnZileSal"
+        Me.btnZileSal.Size = New System.Drawing.Size(150, 23)
+        Me.btnZileSal.TabIndex = 3
+        Me.btnZileSal.Text = "zile pana la salariu"
+        Me.btnZileSal.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(93, 251)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Rezultat:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(144, 59)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Data curenta"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(363, 343)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnZileSal)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnLuna)
+        Me.Controls.Add(Me.DatePicker)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DatePicker As DateTimePicker
+    Friend WithEvents btnLuna As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnZileSal As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub btnLuna_Click(sender As Object, e As EventArgs) Handles btnLuna.Click
+        Dim luna, an, zile As Integer
+        luna = DatePicker.Value.Month
+        an = DatePicker.Value.Year
+        Select Case luna
+            Case 1, 3, 5, 7, 8, 10, 12
+                zile = 31
+            Case 4, 6, 9, 11
+                zile = 30
+            Case 2
+                If ((an Mod 400 = 0 Or an Mod 4 = 0) And an Mod 100 <> 0) Then
+                    zile = 29
+                Else
+                    zile = 28
+                End If
+        End Select
+        TextBox1.Text = zile
+    End Sub
+
+    Private Sub btnZileSal_Click(sender As Object, e As EventArgs) Handles btnZileSal.Click
+        Dim luna, an, zile As Integer
+        luna = DatePicker.Value.Month
+        an = DatePicker.Value.Year
+        Select Case luna
+            Case 1, 3, 5, 7, 8, 10, 12
+                zile = 31
+            Case 4, 6, 9, 11
+                zile = 30
+            Case 2
+                If ((an Mod 400 = 0 Or an Mod 4 = 0) And an Mod 100 <> 0) Then
+                    zile = 29
+                Else
+                    zile = 28
+                End If
+        End Select
+        TextBox1.Text = zile - DatePicker.Value.Day + 1
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
